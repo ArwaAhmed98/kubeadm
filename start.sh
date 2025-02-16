4ee6b256f4010a0a8445bf8739325779ac0a1d33
ArwaAhmed98 <arwaahmedmahmoud99@gmail.com>
2025-02-16 23:21:08 +0200

    join master is working

--- start.sh
+++ start.sh
@@ -1,22 +1,39 @@
 #!/bin/bash
+set -e  # Exit on error
+
+# Debugging
 pwd
 ls
+
+# Create inventory file
 echo "[master]" > inventory.ini
 aws ec2 describe-instances \
     --filters "Name=tag:Name,Values=k8s-master" "Name=instance-state-name,Values=running" \
     --query "Reservations[*].Instances[*].PublicIpAddress" \
-    --output text --region us-east-1 | awk '{print $1, "ansible_host="$1}' >> inventory.ini
+    --output text --region us-east-1 | awk '{print "master ansible_host="$1, "ansible_user=ec2-user ansible_ssh_private_key_file=eks-terraform-key.pem"}' >> inventory.ini
 
 echo "[workers]" >> inventory.ini
 aws ec2 describe-instances \
     --filters "Name=tag:Name,Values=k8s-worker" "Name=instance-state-name,Values=running" \
     --query "Reservations[*].Instances[*].PublicIpAddress" \
-    --output text --region us-east-1 | awk '{print $1, "ansible_host="$1}' >> inventory.ini
+    --output text --region us-east-1 | awk '{print $1, "ansible_host="$1, "ansible_user=ec2-user ansible_ssh_private_key_file=eks-terraform-key.pem"}' >> inventory.ini
 
-echo "[all]" >> inventory.ini
+# Define all children correctly
+echo "[all:children]" >> inventory.ini
+echo "master" >> inventory.ini
+echo "workers" >> inventory.ini
+
+# Re-add [master] group at the end
+echo "" >> inventory.ini
+echo "[master]" >> inventory.ini
 aws ec2 describe-instances \
-    --filters "Name=tag:Name,Values=k8s*" "Name=instance-state-name,Values=running" \
+    --filters "Name=tag:Name,Values=k8s-master" "Name=instance-state-name,Values=running" \
     --query "Reservations[*].Instances[*].PublicIpAddress" \
-    --output text --region us-east-1 | awk '{print $1, "ansible_host="$1}' >> inventory.ini
+    --output text --region us-east-1 | awk '{print "master ansible_host="$1, "ansible_user=ec2-user ansible_ssh_private_key_file=eks-terraform-key.pem"}' >> inventory.ini
 
-ansible-playbook -vvvv -i inventory.ini Ansible/hello.yml --private-key private-key/eks-terraform-key.pem
+# Debug inventory file
+cat inventory.ini
+cd private-key/
+
+# Run Ansible Playbook
+ansible-playbook -i inventory.ini hello.yml --private-key eks-terraform-key.pem
